cbc20e9d87d7d3b81f807ad4419c59c4cbfb4b26
Michael Romulo Luna Abad <lydoived@gmail.com>
2026-07-18 13:01:16 -0500

    feat: implement templates for account management

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     return render(request, 'index.html')
