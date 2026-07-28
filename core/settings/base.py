80362fd8b394ad428278d1739f9add9139de52cb
Michael Romulo Luna Abad <lydoived@gmail.com>
2026-07-28 18:15:02 -0500

    feat: add support dynamic host configurations

--- core/settings/base.py
+++ core/settings/base.py
@@ -15,6 +15,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
+
 
 # Application definition
 INSTALLED_APPS = [
